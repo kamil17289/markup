340c19a663a7e388bfc09c337f8d0481bbfe314f
Kamil Gałkiewicz <kamil@nitro-digital.com>
2019-04-09 10:19:28 +0200

    phpunit as a dev dependency

--- src/Presenters/PlainStringPresenter.php
+++ src/Presenters/PlainStringPresenter.php
@@ -9,6 +9,10 @@ use Nethead\Markup\Html\Tag;
  * @package Nethead\Markup\Presenters
  */
 class PlainStringPresenter implements PresenterInterface {
+    /**
+     * @param Tag $tag
+     * @return string
+     */
     public function present(Tag $tag)
     {
         return (string) $tag;
